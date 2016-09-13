07314746d228eb3a471a3efb49c25b2f977f9319
MarkBeef <s144039@student.dtu.dk>
2016-09-13 16:37:09 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#include <16F887.h>
+#device ADC=16
+
+#device ICD=TRUE
+
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+
+#use delay(internal=8MHz,clock_out)
+
+#include "lcd16216.c"
+
+#use rs232(debugger)
