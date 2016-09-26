3edbdd3ad41276188a8a95dc4010fd5530c33ba2
MarkBeef <s144039@student.dtu.dk>
2016-09-26 20:01:30 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <main.h>
 
+// ADC resolution to get the measured voltage:
 #define resolution 0.01953
 
 long overflow_count;
 #define duty_max 800.0
 
+// Timer interrupt function:
 #int_timer1
 void timer1_isr()
    {
@@ -58,32 +60,45 @@ void main()
          result = adc_voltage*resolution;
          // The duty-cycle is now adjusted to regulate the output voltage
          regulate(&duty, result, goal);
+         // setting the duty cycle to the desired.
          set_pwm1_duty(duty);
         
-          // The 
+          // The updating of the LCD. Updating every second.
           if(overflow_count >= 16) {
+          
+            // Rewriting the duty cycle in percentage:
             duty_per = ((long)duty)/duty_max * 100;
             disable_interrupts(global);
+            
+            // writing on the LCD:
             lcd_clear();
             lcd_gotoxy( 1, 1);
             strcpy (buf, "Duty: "); // Text to display on line 1
             lcd_print(buf);
             sprintf(buf,"%10.3f %%",duty_per);  // place a string in buf that shows time1 with three decimals
             lcd_gotoxy( 1, 2);             // go to start of LCD line 2
-            lcd_print(buf);                // write buf to LCD
+            lcd_print(buf);   // write buf to LCD
+            
+            // Set the overflow_count to zero again:
             overflow_count = 0;
             enable_interrupts(global);
          }
    }  
 }
 
+// Function that regulates the duty cycle. Keeping it at constant voltage.
 void regulate(unsigned int16 *duty, float result, float goal) {
+
+   // If the measured voltage is below the desired goal:
    if((*duty < 800) && (result < goal)) {
       (*duty)++;
    } 
+   
+   // If the measured voltage is higher than the desired goal:
    else if((*duty > 0) && (result > goal)) {
       (*duty)--;
    }
+   // extra reasurence that duty cycles doesn't go above max.
    if(*duty >= 800) {
       duty = 800;
    }
