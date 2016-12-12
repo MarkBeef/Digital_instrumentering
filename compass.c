077fe65b76955e3d70d87bdba4c5304cd524d12a
MarkBeef <s144039@student.dtu.dk>
2016-12-12 12:41:02 +0100

    Update compass.c

--- compass.c
+++ compass.c
@@ -2,6 +2,12 @@ void compass(signed int16 Bx,signed int16 By,signed int16 Bz, signed int16 Gx, s
    
    
    float phi, theta, psi;
+   int16 bxp = Bx;
+   int16 byp = By;
+   int16 bzp = Bz;
+   int16 gxp = Gx;
+   int16 gyp = Gy;
+   int16 gzp = Gz;
   
    
    // Calculating the roll angle Phi radians:
@@ -18,31 +24,42 @@ void compass(signed int16 Bx,signed int16 By,signed int16 Bz, signed int16 Gx, s
      phi = atan((float)Gy/(float)Gz);
    }
    
+   //De-rotating by the roll-angle:
+   float bfy = (float)byp*cos(phi) - (float)bzp*sin(phi);
+   bzp = (float)byp*sin(phi) + (float)bzp*cos(phi);
+   gzp = (float)gyp*sin(phi) + (float)gzp*cos(phi);
+   
    
    // calculating pitch angle Theta in radians:
    
+   /*
    if( Gx > 0) {
       
       theta = Pi/2;
-      
+         
    } else if(Gx < 0) {
       
       theta = -Pi/2;
       
    } else {
+   */
+    //  float denominator = (float)gyp*sin(phi) + (float)gzp*cos(phi);
+      theta = atan( (float)gxp*(-1)/( (float)gzp ));
    
-      float denominator = (float)Gy*sin(phi) + (float)Gz*cos(phi);
-      theta = atan( (float)Gx*(-1)/( denominator ));
+   //}
    
-   }
+   // De-rotate by pitch angle:
+   
+   float bfx = bxp*cos(theta) + bzp*sin(theta);
+   float bfz = -bxp*sin(theta) + bzp*cos(theta);
    
    
    // Calculating yaw:
    
-   if(Bx == 0) {
+   if(bfz == 0) {
       psi = -Pi/2;
    }
    
-   psi = atan( (-1)*(float)By / (float)Bx );
+   psi = atan( (-1)*(float)bfy / (float)bfx );
    
 }
